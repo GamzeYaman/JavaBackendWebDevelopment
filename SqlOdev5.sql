7ac6f9324356d6a6e6eba5d11b796ccedf6c857c
GamzeYaman <71887367+GamzeYaman@users.noreply.github.com>
2021-08-15 13:26:54 +0300

    Create SqlOdev5.sql

---
+++ SqlOdev5.sql
@@ -0,0 +1,3 @@
+1. SELECT * FROM film WHERE title LIKE '%n' ORDER BY title ASC LIMIT 5
+2. SELECT * FROM film WHERE title LIKE '%n' ORDER BY title DESC OFFSET 1 LIMIT 5
+3. SELECT * FROM customer WHERE store_id = 1 ORDER BY last_name DESC LIMIT 4
