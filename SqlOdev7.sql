8e200bbf2aa63d880a1785ecfde86c9be91ee7c9
GamzeYaman <71887367+GamzeYaman@users.noreply.github.com>
2021-08-15 13:50:50 +0300

    Create SqlOdev7.sql

---
+++ SqlOdev7.sql
@@ -0,0 +1,4 @@
+1. SELECT * FROM film GROUP BY rating
+2. SELECT COUNT(film_id), 'replacement_cost' FROM film GROUP BY replacement_cost HAVING COUNT(film_id) > 50
+3. SELECT COUNT(customer_id) FROM customer GROUP BY store_id
+4. SELECT 'country_id', COUNT(city) FROM city GROUP BY country_id ORDER BY city ASC
